2027287ad418a56d9d3f88687dc98f450ab8d5b1
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2024-09-11 10:13:23 -0300

    Foward sub y 6.2

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # type: ignore
 import time
 
 def multiplicar_matriz_vector(A,x):
@@ -16,6 +16,17 @@ def backward_substitution(A, b):
         x[i] = (b[i] - suma) / A[i,i]  
 
     return x     
+
+def foward_substitution(A, b):
+    n = A.shape[0]
+    x = np.zeros(n, dtype=np.float64)
+
+    for i in range(n):
+        suma = 0
+        for j in range(0,i): suma += A[i,j] * x[j]
+        x[i] = (b[i] - suma) / A[i,i]  
+
+    return x  
 
 def eliminacion_gausseana_naive(A, b): 
     m = 0
@@ -67,8 +78,6 @@ def eliminacion_gausseana_pivoteo(A, b):
             for k in range(i, n):
                 A0[j][k] = A0[j][k] - (m * A0[i][k])
 
-        
-
     return backward_substitution(A0,b0)
 
 def factorizar_LU(T):
@@ -83,7 +92,6 @@ def factorizar_LU(T):
             L[j,i] = m
             for k in range(i, n):
                 A0[j,k] = A0[j,k] - (m * A0[i,k])
-
     
     return L, A0
 
@@ -93,8 +101,6 @@ def eliminacion_gausseana_tridiagonal(T,b):
     A = np.zeros(n, dtype=np.float64)
     B = np.zeros(n, dtype=np.float64)
     C = np.zeros(n, dtype=np.float64)
-
-    # TODO: PREGUNTAR DIFERENCIA ENTRE 3.2 Y 3.3 - PREGUNTAR INFORME DE 1 - EXPERIMENTACION
     
     # ai xi−1 + bi xi + ci xi+1 = di
     
@@ -117,6 +123,72 @@ def eliminacion_gausseana_tridiagonal(T,b):
 
     return backward_substitution(T,b)
     
+def factorizar_LU_tri(T):
+    n = T.shape[0]
+    L = np.eye(n, dtype=np.float64)
+    A = np.zeros(n, dtype=np.float64)
+    B = np.zeros(n, dtype=np.float64)
+    C = np.zeros(n, dtype=np.float64)
+
+    # ai xi−1 + bi xi + ci xi+1 = di
+    
+    # Armamos los vectores A B C 
+    for i in range(n):
+        B[i] = T[i][i]
+        A[i] = 0 if i == 0 else T[i][i-1]
+        C[i] = 0 if i == n-1 else T[i][i+1]
+        
+    # Resolvemos
+    for i in range(1, n):
+        coeficiente = A[i] / B[i-1]
+        L[i,i-1] = coeficiente
+        A[i] = A[i] - coeficiente * B[i-1]  # A_i - A_i / B_i-1 * B_i-1    
+        B[i] = B[i] - coeficiente * C[i-1]  # B_i - A_i / B_i-1 * C_i-1
+    
+    for i in range(n):
+        T[i][i] = B[i]
+        if (i >= 1): T[i][i-1] = A[i] 
+        if (i < n-1): T[i][i+1] = C[i]
+
+    return L, T
+
+def generar_laplaciano(n):
+    A = np.zeros((n,n), dtype= np.float64)
+
+    for i in range(n):
+        A[i,i] = -2
+        if i < n-1: A[i,i+1] = 1
+        if i > 0:   A[i,i-1] = 1
+
+    return A
+
+def generar_u_0(n,r,m):
+    u = np.zeros(n, dtype=np.float64)
+
+    lower = n // 2 - r
+    upper = n // 2 + r
+
+    for i in range(lower,upper):
+        u[i] = 1.0
+
+    return u
+
+def calcular_difusion(A,k,r,m):
+    n = A.shape[0]
+    u_0 = generar_u_0(n,r,m)
+    u_1 = np.zeros(n, dtype=np.float64)
+    difusion = [u_0]
+
+    L,U = factorizar_LU_tri(A.copy())
+
+    for _ in range(k):
+        y   = backward_substitution(U,u_0)
+        u_1 = backward_substitution(L, y)
+        difusion.append(u_1)
+
+    return difusion
+
+
 class TestEliminacionGausseana(unittest.TestCase):
     def test_01_resolver_sistema_clase(self):
         A = np.array([
@@ -186,7 +258,7 @@ class TestEliminacionGausseana(unittest.TestCase):
     
         b = np.array([1, 4, 7, 6], dtype=np.float64)
         
-        L, U = factorizar_LU(A)
+        L, U = factorizar_LU(A.copy())
 
         np.testing.assert_array_almost_equal(np.dot(L,U), A, decimal=5)
 
@@ -200,9 +272,33 @@ class TestEliminacionGausseana(unittest.TestCase):
     
         b = np.array([1, 4, 7, 6], dtype=np.float64)
         
-        L, U = factorizar_LU(A)
+        L, U = factorizar_LU(A.copy())
 
-        # COMPLETAR
+        np.testing.assert_array_almost_equal(np.dot(L,U), A, decimal=5)
+
+        y = foward_substitution(L,b)
+        x = backward_substitution(U,y)
+
+        np.testing.assert_array_almost_equal(np.dot(A,x), b, decimal=5) 
+
+    def test_07_difusion(self):
+        alfa = 1
+        k = 10
+        n = 101
+        r = 10
+        m = 1000
+
+        # Matriz laplaciana
+        T = generar_laplaciano(n)
+
+        A = np.eye(n, dtype=np.float64) - alfa * T
+
+        difusiones = calcular_difusion(A.copy(),k,r,m)
+        
+        # El plot que queremos
+        # Y = (np.random.rand(500,50))*2-1
+        # X = np.cumsum(Y,axis=0)
+        # plt.plot(X,'k',alpha=0.3);
 
 def calcular_tiempo_avr(f, l, h, s, cant):
     time_avg = 0
@@ -235,16 +331,16 @@ def calcular_tiempos_naive_vs_pivoteo(l):
     return tiempos_naive, tiempos_pivoteo
 
 if __name__ == "__main__":
-    lista_size = [20,40,60,80,100,120,140,160,180,200]
-    t_n, t_p = calcular_tiempos_naive_vs_pivoteo(lista_size)
-    plt.plot(lista_size, t_n, color = 'blue',label='Eliminacion naive')
-    plt.plot(lista_size, t_p, color = 'red',label='Eliminacion pivoteo')
-    plt.ylabel('Tiempo en segundos')
-    plt.xlabel('Tamaño de la matriz')
+    # lista_size = [20,40,60,80,100,120,140,160,180,200]
+    # t_n, t_p = calcular_tiempos_naive_vs_pivoteo(lista_size)
+    # plt.plot(lista_size, t_n, color = 'blue',label='Eliminacion naive')
+    # plt.plot(lista_size, t_p, color = 'red',label='Eliminacion pivoteo')
+    # plt.ylabel('Tiempo en segundos')
+    # plt.xlabel('Tamaño de la matriz')
 
-    #plt.xscale("log")
-    #plt.yscale("log")
-    plt.legend()
-    plt.show()
-    #unittest.main()
+    # #plt.xscale("log")
+    # #plt.yscale("log")
+    # plt.legend()
+    # plt.show()
+    
     unittest.main()
