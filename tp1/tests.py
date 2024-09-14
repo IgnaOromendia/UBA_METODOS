17adb117dded45673bd4d671630f705bd082d637
Felipe <fmmiriuka@gmail.com>
2024-09-14 12:25:48 -0300

    ej_trid

--- tp1/tests.py
+++ tp1/tests.py
@@ -1,6 +1,35 @@
 import unittest
 from desarrollo import *
 
+def TDMASolve(a, b, c, d):
+    n = len(d)  # número de filas
+    T = np.zeros((n,n))
+
+    for i in range(n):
+        T[i][i] = b[i]
+        if (i >= 1): T[i][i - 1] = a[i]
+        if (i < n - 1): T[i][i + 1] = c[i]
+
+    
+    # Modifica los coeficientes de la primera fila
+    c[0] /= b[0]  # Posible división por cero
+    d[0] /= b[0]
+
+    for i in range(1, n):
+        ptemp = b[i] - (a[i] * c[i-1])
+        c[i] /= ptemp
+        d[i] = (d[i] - a[i] * d[i-1])/ptemp
+
+    # Sustitución hacia atrás
+    x = [0 for i in range(n)]
+    x[-1] = d[-1]
+
+    for i in range(-2, -n-1, -1):
+        x[i] = d[i] - c[i] * x[i+1]
+    
+
+    return np.array(x), T
+
 
 class TestEliminaciongaussiana(unittest.TestCase):
     def test_01_EG_sin_pivoteo(self):
