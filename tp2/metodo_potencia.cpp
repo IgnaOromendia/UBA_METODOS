6b84351c153550b15935e695a968dd9468979fd8
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2024-10-24 18:15:26 -0300

    Implementando deflacion

--- tp2/metodo_potencia.cpp
+++ tp2/metodo_potencia.cpp
@@ -3,25 +3,55 @@
 #include <Eigen/Dense>
 
 using namespace std;
+using namespace Eigen;
 
-using Eigen::MatrixXd;
-using Eigen::VectorXd;
+typedef pair<double, VectorXd> AutoValVec;
 
-typedef pair<vector<double>, vector<vector<double> > > EigensValue;
+struct NOMBRE {
+    vector<double> autovalores;
+    vector<VectorXd> autovectores;
+    vector<int> iteraciones;
+}
 
-void power_iteartion(MatrixXd& A, int niter, double eps) {
+AutoValVec metodo_potencia(MatrixXd& A, int niter, double eps) {
     int n = A.rows();
     double lambda = 1;   
     VectorXd v(n); v.setConstant(-1);
+    VectorXd w = v;
     
     for (int i=0; i < niter; i++){
         VectorXd Av = A * v;
         v = Av / Av.norm();
-        // Criterio
+
+        // Criterio de parada
+        if ((v - w).lpNorm<Infinity>() < eps) break;
+
+        w = v;
     };
 
-    // a = (v.T @ A @ v) / (v.T @ v)
     lambda = (v.transpose() * A * v).value() / (v.transpose() * v).value();    
 
-    //return  
+    return make_pair(lambda, v);
 }
+
+AutoValsVecs obtener_autovalores(MatrixXd& A, int num, int niter=10000, double eps=1e-6) {
+    MatrixXd B = A;
+    vector<double> autovalores;
+    vector<VectorXd> autovectores;
+
+    for (int i = 0; i < num; i++) {
+        AutoValVec res = metodo_potencia(B, niter, eps);
+
+        double l    = res.first;
+        VectorXd v  = res.second; 
+
+        autovalores.push_back(l);
+        autovectores.push_back(v);
+
+        
+        B = B - l * (v * v.transpose());
+    }
+    
+    return AutoValsVecs(autovalores, autovectores);
+}
+
---
+++ tp2/tests.cpp
@@ -0,0 +1,71 @@
+#include "metodo_potencia.cpp"
+#include <cassert>
+#include <string>
+
+// Para compilar y ejecutar
+// g++ -w -O3 -std=c++17 -I ./eigen-3.4.0/ tests.cpp -o ejecTest;./ejecTest
+
+MatrixXd matriz_householder(VectorXd w) {
+    int n = w.size();
+    MatrixXd D = MatrixXd::Zero(n,n);
+
+    D.diagonal() = w;
+
+    VectorXd v(n); v.setConstant(1);
+
+    v = v / v.lpNorm<2>();
+
+    MatrixXd H = MatrixXd::Identity(n,n) - 2 * (v * v.transpose());
+
+    return H * D * H.transpose();
+}
+
+void test01_metodo_potencia() {
+    VectorXd v(5); v << 5.0, 4.0, 3.0, 2.0, 1.0;
+    MatrixXd A = matriz_householder(v);
+
+    AutoValVec result = metodo_potencia(A, 10000, 1e-20);
+
+    assert(abs(result.first - 5.0) < 1e-6);
+
+    cout << "Test 1 Método Potencia OK" << endl;
+}
+
+void test02_deflacion_diagonal() {
+    VectorXd v(5); v << 5.0, 4.0, 3.0, 2.0, 1.0;
+    MatrixXd D = MatrixXd::Zero(5, 5);
+    D.diagonal() = v;
+    
+    AutoValsVecs res = obtener_autovalores(D, 5);
+
+    for(int i = 0; i < 5; i++) {
+        double l_esperado = v(i);
+        double l_obtenido = res.first[i];
+        // cout << "Obtenido " + to_string(l_obtenido) + " vs Esperado " + to_string(l_esperado) << endl;
+        assert(abs(l_obtenido - l_esperado) < 1e-6);
+    }
+    
+    cout << "Test 2 Deflación con Diagonal OK" << endl;
+}
+
+void test03_deflacion_householder(){
+    VectorXd v(10); v << 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0;
+    MatrixXd B = matriz_householder(v);
+
+    AutoValsVecs res = obtener_autovalores(B, 10);
+
+    for(int i = 0; i < 10; i++) {
+        double l_esperado = v(i);
+        double l_obtenido = res.first[i];
+        // cout << "Obtenido " + to_string(l_obtenido) + " vs Esperado " + to_string(l_esperado) << endl;
+        assert(abs(l_obtenido - l_esperado) < 1e-6);
+    }
+    
+    cout << "Test 3 Deflación con Householder OK" << endl;
+}
+
+int main() {
+    test01_metodo_potencia();
+    test02_deflacion_diagonal();
+    test03_deflacion_householder();
+}
