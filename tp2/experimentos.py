b5ca193bacf8d7be5d0470d6b02e421ccda36b0e
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2024-10-26 16:05:21 -0300

    Convergencia y error

--- tp2/experimentos.py
+++ tp2/experimentos.py
@@ -1,58 +1,65 @@
 import matplotlib.pyplot as plt
 import subprocess as sp
 import numpy as np
+import csv
 
-def escribir_matriz(matrices, archivo):
+def escribir_matriz(mat_a_escribir, archivo):
     with open(archivo, 'w') as f:
-        f.write(str(len(matrices)) + "\n")
-        for A in matrices:
-            f.write(str(len(A)) + " " + str(len(A[0])) + "\n")
-            for i in range(len(A)):
-                for j in range(len(A[0])):
-                    f.write(str(A[i][j]) + " ")
-                f.write("\n")
+        f.write(str(len(mat_a_escribir)) + "\n")
+        f.write(str(len(mat_a_escribir[0])) + "\n")
+
+        for matrices in mat_a_escribir:
+            for A in matrices:
+                f.write(str(len(A)) + " " + str(len(A[0])) + "\n")
+                for i in range(len(A)):
+                    for j in range(len(A[0])):
+                        f.write(str(A[i][j]) + " ")
+                    f.write("\n")
 
 def leer_resultados_mp():
-    with open("output_mp.out", 'r') as f:
-        lines = f.readlines()
+    reps = 5
+    with open("output_mp.csv", 'r') as f:
+        reader = csv.reader(f)
         
-        cant_mat = int(lines[0].split()[0])
-        cant_autoval = int(lines[1].split()[0]) 
-        l = int(lines[2].split()[0]) # Cantidad de lineas a leer
+        next(reader) # Salteamos los headings
 
+        mat_id = 1
+        autovals = []
+        autovecs = []
+        it_prom = []
+        it_des = []
+        err_prom = []
+        err_des = []
         datos_matrices = []
 
-        for k in range(1, cant_mat+1):
-            line_idx = l * (k-1) + 3;
+        for linea in reader:
+            new_mat_id = int(linea[0])
 
-            autovalores = []
-            autovectores = []
-            iteraciones = []
-            error = []
+            if mat_id != new_mat_id:
+                datos_matrices.append((autovals.copy(), autovecs.copy(), it_prom.copy(), it_des.copy(), err_prom.copy(), err_des.copy()))
+                mat_id = new_mat_id
+                autovecs.clear()
+                autovals.clear()
+                it_prom.clear()
+                it_des.clear()
+                err_prom.clear()
+                err_des.clear()
+                
+            autovals.append(float(linea[1]))
+            autovecs.append(np.array([float(i) for i in linea[2:-4]]))
+            it_prom.append(float(linea[-4]))
+            it_des.append(float(linea[-3]))
+            err_prom.append(float(linea[-2]))
+            err_des.append(float(linea[-1]))
 
-            i = line_idx 
-
-            while i < l + line_idx:
-                # print(i)
-                data = lines[i].split()
-                # print(data)
-                autovalores.append(float(data[0]))
-                iteraciones.append(int(data[1]))
-                error.append(float(data[2]))
-                v = []
-                for j in range(1,cant_autoval):
-                    v.append(float(lines[i + j].split()[0]))
-                autovectores.append(v)
-                i += cant_autoval + 1
-
-            datos_matrices.append((autovalores, autovectores, iteraciones, error))
+        datos_matrices.append((autovals.copy(), autovecs.copy(), it_prom.copy(), it_des.copy(), err_prom.copy(), err_des.copy())) # Agregamos los datos de la última matriz
 
     return datos_matrices
 
-def calular_autovalores(archivo, num):
-    resultado = sp.run(['./ejecMP', archivo, str(num)], text=True)
+def calular_autovalores(archivo):
+    resultado = sp.run(['./ejecMP', archivo], text=True)
     if resultado.returncode == 0:
-        print("Datos en output_mp.out")
+        print("Datos en output_mp.csv")
     else:
         print("Error:", resultado.stderr)
 
@@ -60,26 +67,31 @@ def plot_convergencia(resultados, epsilons):
     cant_lambdas = len(resultados[0][0])
 
     for i in range(cant_lambdas):
-        plt.plot(epsilons, resultados[i][2], label=f'$\lambda_{{{i+1}}}$')
+        plt.errorbar(epsilons, resultados[i][2], yerr=resultados[i][3], label=f'$\lambda_{{{i+1}}}$')
+        plt.scatter(epsilons, resultados[i][2])
 
     plt.ylabel('Iteraciones')
     plt.xlabel('$\epsilon$')
     plt.xscale("log")
+    plt.yscale("log")
     plt.legend()
-    # plt.savefig('graficos/grafico_convergencia.png')
+    plt.grid(True)
+    plt.savefig('graficos/grafico_convergencia.png')
     plt.show()
 
-# plt.errorbar para error de aprox
-def error_metodo(lambdas, errores, epsilons):
-    for i in range(len(lambdas)):
-        plt.plot(errores[i], epsilons[i], label='$\lambda_{i}$')
+def plot_error_metodo_potencia(resultados, epsilons):
+    cant_lambdas = len(resultados[0][0])
+
+    for i in range(cant_lambdas):
+        plt.errorbar(epsilons, resultados[i][4], yerr=resultados[i][5], label=f'$\lambda_{{{i+1}}}$')
+        plt.scatter(epsilons, resultados[i][4])
 
     plt.ylabel('Error')
     plt.xlabel('$\epsilon$')
-    plt.xscale("log", base=2)
-    plt.yscale("log")
+    plt.xscale("log")
     plt.legend()
-    # plt.savefig('graficos/grafico_convergencia.png')
+    plt.grid(True)
+    plt.savefig('graficos/grafico_error_metodo_potencia.png')
     plt.show()
 
 def matriz_householder(w):
@@ -100,16 +112,22 @@ if __name__ == "__main__":
     archivo = 'input_mp.dat'
 
     epsilons = np.logspace(-4, 0,num=5)
-    matrices = []
+    matrices_a_escribir = []
+
+    matrices_por_eps = 5
 
     for eps in epsilons:
         v = [10, 10 - eps, 5, 2, 1]
-        matrices.append(matriz_householder(v))
+        matrices_para_eps = []
+        for _ in range(matrices_por_eps):
+            matrices_para_eps.append(matriz_householder(v))
+        matrices_a_escribir.append(matrices_para_eps)
 
-    escribir_matriz(matrices, archivo)
+    escribir_matriz(matrices_a_escribir, archivo)
     
-    calular_autovalores(archivo,len(v))
+    calular_autovalores(archivo)
 
     resultados_mp = leer_resultados_mp()
     
-    plot_convergencia(resultados_mp, epsilons)
+    # plot_convergencia(resultados_mp, epsilons)
+    plot_error_metodo_potencia(resultados_mp, epsilons)
--- tp2/metodo_potencia.cpp
+++ tp2/metodo_potencia.cpp
@@ -12,17 +12,38 @@ struct AutoData {
     double l;
     VectorXd v;
     int it;
+    double it_promedio = 0;
+    double it_desvio = 0;
     double err;
+    double err_promedio = 0;
+    double err_desvio = 0;
     AutoData(double autoval, VectorXd autovec, int i, double error): l(autoval), v(autovec), it(i), err(error) {}
 };
 
-AutoData metodo_potencia(MatrixXd& A, int niter, double eps) {
-    int n = A.rows();
-    double l = 1;   
-    VectorXd v(n); v.setConstant(-1);
-    VectorXd w = v;
-    int i=0;
+double calcular_promedio(vector<double>& v) {
+    double suma = 0;
+
+    for(double e: v)
+        suma += e;
+
+    return suma / v.size();
+}
+
+double calcular_desvio(vector<double>& v, double promedio) {
+    double sumaCuadrados = 0.0;
+
+    for (double e : v) 
+        sumaCuadrados += pow(e - promedio, 2);
     
+    return sqrt(sumaCuadrados / v.size());
+}
+
+AutoData metodo_potencia(MatrixXd& A, int niter, double eps, int reps) {
+    double l = 1;   
+    VectorXd v(A.rows()); v.setConstant(-1);
+    VectorXd w = v;
+    int i = 0;
+
     for (; i < niter; i++){
         VectorXd Av = A * v;
         v = Av / Av.norm();
@@ -31,7 +52,7 @@ AutoData metodo_potencia(MatrixXd& A, int niter, double eps) {
         if ((v - w).lpNorm<Infinity>() < eps) break;
         
         w = v;
-    };
+    }
 
     l = (v.transpose() * A * v).value() / (v.transpose() * v).value();    
 
@@ -40,12 +61,12 @@ AutoData metodo_potencia(MatrixXd& A, int niter, double eps) {
     return AutoData(l, v, i, err);
 }
 
-vector<AutoData> obtener_autovalores(MatrixXd& A, int num, int niter=10e8, double eps=10e-7) {
+vector<AutoData> obtener_autovalores(MatrixXd& A, int niter=10e8, double eps=10e-7, int reps=5) {
     MatrixXd B = A;
     vector<AutoData> result;
 
-    for (int i = 0; i < num; i++) {
-        AutoData res = metodo_potencia(B, niter, eps);
+    for (int i = 0; i < B.rows(); i++) {
+        AutoData res = metodo_potencia(B, niter, eps, reps);
         result.push_back(res);
         B = B - res.l * (res.v * res.v.transpose());
     }
@@ -53,26 +74,28 @@ vector<AutoData> obtener_autovalores(MatrixXd& A, int num, int niter=10e8, doubl
     return result;
 }
 
-void escribir_output(vector<vector<AutoData>>& resultados) {
-    ofstream f("output_mp.out");
+void escribir_vector(ofstream& f, VectorXd& vec) {
+    for (int i = 0; i < vec.size(); ++i) 
+        f << vec(i) << ", ";
+}
 
-    int cant_autovalores = resultados[0].size();
+void escribir_output(vector<vector<AutoData>>& res) {
+    ofstream f("output_mp.csv");
 
-    f << resultados.size() << endl;
-    f << cant_autovalores << endl;
-    f << cant_autovalores + (resultados[0][0].v.size() * cant_autovalores) << endl;
+    f << "Matriz,Autovalor,Autovector...,Iteraciones...,Error\n";
 
-    for(auto& res: resultados) {
-        for (int i = 0; i < res.size(); i++) {
-            f << res[i].l << " " << res[i].it << " " << res[i].err << endl;
-            f << res[i].v << endl;
+    for(int k = 0; k < res.size(); k++) {
+        for (int i = 0; i < res[k].size(); i++) {
+            f << k + 1 << ", " <<  res[k][i].l << ", ";
+            escribir_vector(f, res[k][i].v);
+            f << res[k][i].it_promedio << ", " << res[k][i].it_desvio << ", " << res[k][i].err_promedio << ", " << res[k][i].err_desvio << endl;
         }
     }
     
     f.close();
 }
 
-vector<MatrixXd> leer_input(string nombre_archivo) {
+vector<vector<MatrixXd>> leer_input(string nombre_archivo) {
     ifstream f(nombre_archivo);
 
     if (!f) {
@@ -81,22 +104,31 @@ vector<MatrixXd> leer_input(string nombre_archivo) {
     }
 
     int cant_mat; f >> cant_mat;
-    vector<MatrixXd> result;
+    int cant_mat_por_eps; f >> cant_mat_por_eps;
+
+    vector<vector<MatrixXd>> result(cant_mat);
 
     for (int i = 0; i < cant_mat; i++) {
-        int filas, columnas;
+        vector<MatrixXd> mat_por_eps(cant_mat_por_eps);
 
-        f >> filas >> columnas;
+        for (int k = 0; k < cant_mat_por_eps; k++) { 
+            int filas, columnas;
 
-        MatrixXd M(filas, columnas);
+            f >> filas >> columnas;
 
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                f >> M(i, j);  
+            MatrixXd M(filas, columnas);
+
+            for (int i = 0; i < filas; i++) {
+                for (int j = 0; j < columnas; j++) {
+                    f >> M(i, j);  
+                }
             }
-        }
 
-        result.push_back(M);
+            mat_por_eps[k] = M;
+        }
+        
+
+        result[i] = mat_por_eps;
     }
 
     return result;
@@ -106,17 +138,47 @@ vector<MatrixXd> leer_input(string nombre_archivo) {
 // g++ -w -O3 -std=c++17 -I ./eigen-3.4.0/ metodo_potencia.cpp -o ejecMP
 
 int main(int argc, char** argv) {
-    if (argc < 3) {
+    if (argc < 2) {
         cerr << "Falta el archivo o cantidad de autovalores" << endl;
         return 1;
     }
 
-    vector<MatrixXd> matrices = leer_input(argv[1]);
+    vector<vector<MatrixXd>> matrices = leer_input(argv[1]);
     
     vector<vector<AutoData>> results;
 
-    for(MatrixXd A: matrices)
-        results.push_back(obtener_autovalores(A, stod(argv[2])));
+    // Por cada matriz a experimentar vamos a promediar y calcular el desvio de todas las iteraciones y errores
+    for(int i = 0; i < matrices.size(); i++) {
+        vector<vector<double>> iteraciones(matrices[0][0].size()); // Iteraciones por cada lambda
+        vector<vector<double>> errores(matrices[0][0].size()); // Errores por cada lambda
+        
+        vector<AutoData> autodata_por_eps; // Representante de cada eps (ya que todos tinenen los mismo autovalores)
+
+        for(int k = 0; k < matrices[i].size(); k++) {
+            vector<AutoData> data = obtener_autovalores(matrices[i][k]);
+            
+            // Guardamos las iteraciones de cada lambda
+            for(int l = 0; l < data.size(); l++) {
+                iteraciones[l].push_back(data[l].it);
+                errores[l].push_back(data[l].err);
+            }
+                
+            // Guardamos representate
+            if (k == 0) autodata_por_eps = data; 
+        }
+
+        for(int l = 0; l < autodata_por_eps.size(); l++) {
+            autodata_por_eps[l].it_promedio = calcular_promedio(iteraciones[l]);
+            autodata_por_eps[l].it_desvio   = calcular_desvio(iteraciones[l], autodata_por_eps[l].it_promedio);
+
+            autodata_por_eps[l].err_promedio = calcular_promedio(errores[l]);
+            autodata_por_eps[l].err_desvio   = calcular_desvio(errores[l], autodata_por_eps[l].err_promedio);
+        }
+
+        results.push_back(autodata_por_eps);
+        
+    }
+        
 
     escribir_output(results);
 }
