0055ec80ea705c5dfc0cebc6a93371c8d6665408
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2024-10-29 19:04:39 -0300

    PCA

--- tp2/desarrollo.py
+++ tp2/desarrollo.py
@@ -2,16 +2,111 @@ import numpy as np
 import pandas as pd
 from scipy import stats #type: ignore
 import subprocess as sp
+import csv
 
 mapMovieIndex = {"science fiction": 0, "romance":1, "crime":2, "western":3}
 mapIndexMovie = {0:"science fiction", 1:"romance", 2:"crime", 3:"western"}
 
-def calular_autovalores(nombre_archivo):
-    resultado = sp.run(['./ejecMP', nombre_archivo], text=True)
+def escribir_input_experimento_mp(mat_a_escribir, archivo):
+    with open(archivo, 'w') as f:
+        f.write(str(len(mat_a_escribir)) + "\n")
+        f.write(str(len(mat_a_escribir[0])) + "\n")
+
+        for matrices in mat_a_escribir:
+            for A in matrices:
+                f.write(str(len(A)) + " " + str(len(A[0])) + "\n")
+                for i in range(len(A)):
+                    for j in range(len(A[0])):
+                        f.write(str(A[i][j]) + " ")
+                    f.write("\n")
+
+def escribir_input_mp(matrices, archivo):
+    with open(archivo, 'w') as f:
+        f.write(str(len(matrices)) + "\n")
+        
+        for A in matrices:
+            f.write(str(A.shape[0]) + " " + str(A.shape[1]) + "\n")
+            for i in range(A.shape[0]):
+                for j in range(A.shape[1]):
+                    f.write(str(A[i][j]) + " ")
+                f.write("\n")
+
+def leer_output_experimento_mp(fileName):
+    with open(fileName, 'r') as f:
+        reader = csv.reader(f)
+        
+        next(reader) # Salteamos los headings
+
+        mat_id = 1
+        autovals = []
+        autovecs = []
+        it_prom = []
+        it_des = []
+        err_prom = []
+        err_des = []
+        datos_matrices = []
+
+        for linea in reader:
+            new_mat_id = int(linea[0])
+
+            if mat_id != new_mat_id:
+                datos_matrices.append((autovals.copy(), autovecs.copy(), it_prom.copy(), it_des.copy(), err_prom.copy(), err_des.copy()))
+                mat_id = new_mat_id
+                autovecs.clear()
+                autovals.clear()
+                it_prom.clear()
+                it_des.clear()
+                err_prom.clear()
+                err_des.clear()
+                
+            autovals.append(float(linea[1]))
+            autovecs.append(np.array([float(i) for i in linea[2:-4]]))
+            it_prom.append(float(linea[-4]))
+            it_des.append(float(linea[-3]))
+            err_prom.append(float(linea[-2]))
+            err_des.append(float(linea[-1]))
+
+        datos_matrices.append((autovals.copy(), autovecs.copy(), it_prom.copy(), it_des.copy(), err_prom.copy(), err_des.copy())) # Agregamos los datos de la última matriz
+
+    return datos_matrices
+
+def leer_output_mp(fileName):
+    with open(fileName, 'r') as f:
+        reader = csv.reader(f)
+        
+        next(reader) # Salteamos los headings
+
+        mat_id = 1
+        autovals = []
+        autovecs = []
+        datos_matrices = []
+
+        for linea in reader:
+            new_mat_id = int(linea[0])
+
+            if mat_id != new_mat_id:
+                datos_matrices.append((autovals.copy(), autovecs.copy()))
+                mat_id = new_mat_id
+                autovecs.clear()
+                autovals.clear()
+                
+            autovals.append(float(linea[1]))
+            autovecs.append(np.array([float(i) for i in linea[2:-1]]))
+            
+
+        datos_matrices.append((autovals.copy(), autovecs.copy())) # Agregamos los datos de la última matriz
+
+    return datos_matrices
+
+def calular_autovalores(nombre_archivo, output_name, experimento):
+    resultado = sp.run(['./ejecMP', nombre_archivo, output_name, experimento], text=True)
     if resultado.returncode == 0:
-        print("Datos en output_mp.csv")
+        print("Datos en " + output_name)
     else:
         print("Error:", resultado.stderr)
+
+def separarAutoData(resultado):
+    return np.array(resultado[0][0]), np.array(resultado[0][1])
 
 def matriz_covarianza(X):
     return X.T @ X / (X.shape[0] - 1)
@@ -35,10 +130,7 @@ def dist_coseno(X, Y):
     Y_norm = np.linalg.norm(Y, axis=1, ord=2, keepdims=True)
 
     with np.errstate(divide='ignore', invalid='ignore'):
-        # Calcular la similitud coseno
         cos_sim = (X @ Y.T) / (X_norm @ Y_norm.T)
-
-        # Reemplazar NaN y -inf/inf por 0
         cos_sim = np.nan_to_num(cos_sim, nan=0.0, posinf=0.0, neginf=0.0)
 
     return 1 - cos_sim
@@ -111,7 +203,7 @@ def obtener_particion(df, cant_particiones):
         particion_t = []
 
         for _ in  range(tam_particion):
-            (i1, crime_row), (i2, western_row), (i3, romance_row), (i4, science_row) = zippedFrame.pop(0)
+            (_, crime_row), (_, western_row), (_, romance_row), (_, science_row) = zippedFrame.pop(0)
             science_row['nuevo_idx'] = len(particion_t) 
             particion_t.append(science_row)
             
@@ -131,7 +223,7 @@ def obtener_particion(df, cant_particiones):
 
     return particiones
         
-def cross_validation(k, cant_partes, dataFrame, Q):
+def knn_cross_validation(k, cant_partes, dataFrame, Q):
     lista_particiones = obtener_particion(dataFrame, cant_partes)
 
     performance_cross_v = []
@@ -153,6 +245,28 @@ def cross_validation(k, cant_partes, dataFrame, Q):
     
     return suma / len(performance_cross_v)
     
+def pca_cross_validation(p, cant_partes, dataFrame, Q):
+    lista_particiones = obtener_particion(dataFrame, cant_partes)
+
+    varianzas = []
+    
+    for i in range(cant_partes):
+        lista_train_set = []
+        for j in range(cant_partes):
+            if (i != j):
+                lista_train_set.append(lista_particiones[j])
+    
+        test_set = lista_particiones[i]
+        train_set = pd.concat(lista_train_set, axis=0)        
+
+        varianzas.append(pca(train_set, Q, p))
+
+    suma = 0.0
+    for var in varianzas:
+        suma += var
+    
+    return suma / len(varianzas)
+
 def explorar_parametro_k(cant_partes, dataFrame, Q):
     k_sample = [i for i in range(1,len(dataFrame))]
 
@@ -162,13 +276,31 @@ def explorar_parametro_k(cant_partes, dataFrame, Q):
     max_perf = 0
 
     for k in k_sample:
-        perf_k = cross_validation(k, cant_partes, dataFrame, Q)
+        perf_k = knn_cross_validation(k, cant_partes, dataFrame, Q)
         if perf_k > max_perf:
             max_k = k
             max_perf = perf_k
 
     return max_k
-        
+
+def explorar_parametro_p(cant_partes, dataFrame, Q):
+    p_sample = [i for i in range(1,len(dataFrame),5)]
+
+    dataFrame = dataFrame[dataFrame["split"] == "train"].reset_index()
+
+    mejor_p = None
+    var_acumulada = []
+
+    for p in p_sample:
+        var_acumulada = pca_cross_validation(p, cant_partes, dataFrame, Q)
+
+        print(p, var_acumulada[-1])
+        if np.any(var_acumulada >= 0.95):
+            mejor_p = p
+            break  
+
+    return mejor_p, var_acumulada
+
 # Este es el 3a
 def calsificar_con_distinta_cantidad_de_toknes():
     train_set = df[df["split"] == "train"].reset_index()
@@ -178,17 +310,37 @@ def calsificar_con_distinta_cantidad_de_toknes():
     print(clasificador_de_genero(Q=1000, k=5, train_set=train_set,test_set=test_set))
     print(clasificador_de_genero(Q=5000, k=5, train_set=train_set,test_set=test_set))
 
-def preprocesamiento_PCA(dataFrame, Q):
-    train_set = df[df["split"] == "train"].reset_index()
+def pca(dataFrame, Q, p):
+    train_set = dataFrame[dataFrame["split"] == "train"].reset_index()
     
     X_train = matriz_tokens(Q, train_set)
     
     X_train_centrado = X_train - X_train.mean(0)
 
-    C_train = (X_train_centrado.T @ X_train_centrado) / (len(X_train_centrado) - 1)
+    C_train = matriz_covarianza(X_train_centrado)
+
+    # Calcular autovalores
+    escribir_input_mp([C_train],"pca_input.dat")
+    
+    # calular_autovalores("pca_input.dat", "pca_output.csv", "0")
+
+    w, V = separarAutoData(leer_output_mp("pca_output.csv"))
+
+    indices = np.argsort(w)[::-1]
+
+    w = w[indices]
+    V = V[:,indices]
+
+    # Cambio de base
+    # Xhat = X_train_centrado @ V
+
+    var_acumulada = np.cumsum(w) / np.sum(w) 
+
+    return var_acumulada[:p]
+
 
 # Leer datos:                   
 df = pd.read_csv("datos.csv")
 df["GenreID"] = df["Genre"].apply(lambda x: mapMovieIndex[x])
 
-calsificar_con_distinta_cantidad_de_toknes()
+explorar_parametro_p(5,df,500)
--- tp2/metodo_potencia.cpp
+++ tp2/metodo_potencia.cpp
@@ -8,7 +8,7 @@ using namespace Eigen;
 
 typedef pair<double, VectorXd> AutoValVec;
 
-struct AutoData {
+struct autoData {
     double l;
     VectorXd v;
     int it;
@@ -17,7 +17,7 @@ struct AutoData {
     double err;
     double err_promedio = 0;
     double err_desvio = 0;
-    AutoData(double autoval, VectorXd autovec, int i, double error): l(autoval), v(autovec), it(i), err(error) {}
+    autoData(double autoval, VectorXd autovec, int i, double error): l(autoval), v(autovec), it(i), err(error) {}
 };
 
 double calcular_promedio(vector<double>& v) {
@@ -38,7 +38,7 @@ double calcular_desvio(vector<double>& v, double promedio) {
     return sqrt(sumaCuadrados / v.size());
 }
 
-AutoData metodo_potencia(MatrixXd& A, int niter, double eps) {
+autoData metodo_potencia(MatrixXd& A, int niter, double eps) {
     double l = 1;   
     VectorXd v(A.rows()); v.setConstant(-1);
     VectorXd w = v;
@@ -57,18 +57,19 @@ AutoData metodo_potencia(MatrixXd& A, int niter, double eps) {
     l = (v.transpose() * A * v).value() / (v.transpose() * v).value();    
 
     double err = (A * v - l * v).lpNorm<2>();
-    
-    return AutoData(l, v, i, err);
+        
+    return autoData(l, v, i, err);
 }
 
-vector<AutoData> obtener_autovalores(MatrixXd& A, int niter=10e8, double eps=10e-7) {
+vector<autoData> obtener_autovalores(MatrixXd& A, int niter=10e3, double eps=10e-4) { //TODO acordarse de cambiar la cantidad de iteraciones y el eps
     MatrixXd B = A;
-    vector<AutoData> result;
+    vector<autoData> result;
 
     for (int i = 0; i < B.rows(); i++) {
-        AutoData res = metodo_potencia(B, niter, eps);
+        autoData res = metodo_potencia(B, niter, eps);
         result.push_back(res);
         B = B - (res.l * res.v * res.v.transpose());
+        cout << "lamda " << i+1 << " en " << res.it << " err: " << res.err << endl;
     }
     
     return result;
@@ -79,23 +80,60 @@ void escribir_vector(ofstream& f, VectorXd& vec) {
         f << vec(i) << ", ";
 }
 
-void escribir_output(vector<vector<AutoData>>& res) {
-    ofstream f("output_mp.csv");
+// Calculo de autovalores
+vector<MatrixXd> leer_input(string nombre_archivo) {
+    ifstream f(nombre_archivo);
 
-    f << "Matriz,Autovalor,Autovector...,IterPromedio,IterDesivo,ErrorPromedio,ErrorDesvio\n";
+    if (!f) {
+        cerr << "Error en el archivo";
+        exit(1);
+    }
+
+    int cant_mat; f >> cant_mat;
+
+    vector<MatrixXd> result(cant_mat);
+
+    for (int i = 0; i < cant_mat; i++) {
+        int filas, columnas; f >> filas >> columnas;
+
+        MatrixXd M(filas, columnas);
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                f >> M(i, j);  
+            }
+        }
+
+        result[i] = M;
+    }
+
+    return result;
+}
+
+void procesar_matrices(vector<MatrixXd>& matrices, vector<vector<autoData>>& results) {
+    // Por cada matriz a procesar vamos caluclar los autovalores y autovectores
+    for(int i = 0; i < matrices.size(); i++) 
+        results.push_back(obtener_autovalores(matrices[i]));   
+}
+
+void escribir_output(vector<vector<autoData>>& res, string nombre_archivo) {
+    ofstream f(nombre_archivo);
+
+    f << "Matriz,Autovalor,Autovector...,\n";
 
     for(int k = 0; k < res.size(); k++) {
         for (int i = 0; i < res[k].size(); i++) {
             f << k + 1 << ", " <<  res[k][i].l << ", ";
             escribir_vector(f, res[k][i].v);
-            f << res[k][i].it_promedio << ", " << res[k][i].it_desvio << ", " << res[k][i].err_promedio << ", " << res[k][i].err_desvio << endl;
+            f << endl;
         }
     }
     
     f.close();
 }
 
-vector<vector<MatrixXd>> leer_input(string nombre_archivo) {
+// Experimentación
+vector<vector<MatrixXd>> leer_input_para_experimentar(string nombre_archivo) {
     ifstream f(nombre_archivo);
 
     if (!f) {
@@ -112,9 +150,7 @@ vector<vector<MatrixXd>> leer_input(string nombre_archivo) {
         vector<MatrixXd> mat_por_eps(cant_mat_por_eps);
 
         for (int k = 0; k < cant_mat_por_eps; k++) { 
-            int filas, columnas;
-
-            f >> filas >> columnas;
+            int filas, columnas; f >> filas >> columnas;
 
             MatrixXd M(filas, columnas);
 
@@ -134,28 +170,16 @@ vector<vector<MatrixXd>> leer_input(string nombre_archivo) {
     return result;
 }
 
-// Para compilar
-// g++ -w -O3 -std=c++17 -I ./eigen-3.4.0/ metodo_potencia.cpp -o ejecMP
-
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        cerr << "Falta el archivo o cantidad de autovalores" << endl;
-        return 1;
-    }
-
-    vector<vector<MatrixXd>> matrices = leer_input(argv[1]); // Por cada epsilon un vector de Matrices
-    
-    vector<vector<AutoData>> results;
-
+void procesar_matrices_para_experimentar(vector<vector<MatrixXd>>& matrices, vector<vector<autoData>>& results) {
     // Por cada matriz a experimentar vamos a promediar y calcular el desvio de todas las iteraciones y errores
     for(int i = 0; i < matrices.size(); i++) {
         vector<vector<double>> iteraciones(matrices[0][0].size()); // Iteraciones por cada lambda
         vector<vector<double>> errores(matrices[0][0].size()); // Errores por cada lambda
         
-        vector<AutoData> autodata_por_eps; // Representante de cada eps (ya que todos tinenen los mismo autovalores)
+        vector<autoData> autodata_por_eps; // Representante de cada eps (ya que todos tinenen los mismo autovalores)
 
         for(int k = 0; k < matrices[i].size(); k++) {
-            vector<AutoData> data = obtener_autovalores(matrices[i][k]);
+            vector<autoData> data = obtener_autovalores(matrices[i][k]);
             
             // Guardamos las iteraciones de cada lambda
             for(int l = 0; l < data.size(); l++) {
@@ -178,7 +202,46 @@ int main(int argc, char** argv) {
         results.push_back(autodata_por_eps);
         
     }
-        
+}
 
-    escribir_output(results);
+void escribir_output_experimento(vector<vector<autoData>>& res, string nombre_archivo) {
+    ofstream f(nombre_archivo);
+
+    f << "Matriz,Autovalor,Autovector...,IterPromedio,IterDesivo,ErrorPromedio,ErrorDesvio\n";
+
+    for(int k = 0; k < res.size(); k++) {
+        for (int i = 0; i < res[k].size(); i++) {
+            f << k + 1 << ", " <<  res[k][i].l << ", ";
+            escribir_vector(f, res[k][i].v);
+            f << res[k][i].it_promedio << ", " << res[k][i].it_desvio << ", " << res[k][i].err_promedio << ", " << res[k][i].err_desvio << endl;
+        }
+    }
+    
+    f.close();
+}
+
+// Para compilar
+// g++ -w -O3 -std=c++17 -I ./eigen-3.4.0/ metodo_potencia.cpp -o ejecMP
+
+int main(int argc, char** argv) {
+    if (argc < 4) {
+        cerr << "Falta el archivo o cantidad de autovalores" << endl;
+        return 1;
+    }
+
+    string input_file  = argv[1]; 
+    string output_file = argv[2];
+    bool experimentar  = string(argv[3]) == "1";
+
+    if (experimentar) {
+        vector<vector<MatrixXd>> matrices = leer_input_para_experimentar(input_file); // Por cada epsilon un vector de Matrices
+        vector<vector<autoData>> results;
+        procesar_matrices_para_experimentar(matrices, results);
+        escribir_output_experimento(results, output_file);
+    } else {
+        vector<MatrixXd> matrices = leer_input(input_file);
+        vector<vector<autoData>> results;
+        procesar_matrices(matrices, results);
+        escribir_output(results, output_file);
+    }    
 }
