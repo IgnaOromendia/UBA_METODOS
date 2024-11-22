8addbc410ea412a7bbb610915cdefa0679ca4a3d
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2024-11-22 19:04:22 -0300

    Tp3

--- tp3/experimentos.py
+++ tp3/experimentos.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import multiprocessing as mp
 import desarrollo as ds
+import numpy as np
 
 def plot_error_sujeto_1():
     grados, err_ajuste, err_val = ds.predecir_sin_reg(sujeto=1)
@@ -16,33 +17,38 @@ def plot_error_sujeto_1():
     plt.show()
 
 def heatmap_sujetos(error, sujeto, ax):
+    lambdas = np.logspace(1e-8,1, 100)
+    lambda_range = range(1,len(lambdas),10)
+
     cax = ax.pcolor(error, cmap='hot', shading='auto') 
     plt.colorbar(cax, ax=ax, label='Error')
-    
+
+    ax.set_xticks(np.arange(1,len(lambdas),10) + 0.5)
+    ax.set_xticklabels([f'$\lambda_{{{i+1}}}$' for i in lambda_range], ha='right')
+
     ax.set_title(f'Mapa de Calor del error del sujeto nº {sujeto}')
     ax.set_ylabel('Grado')
-    ax.set_xlabel('$\lambda$')
 
 def explorar_en_sujeto(sujeto):
     x_aju, y_aju = ds.leer_datos('./datos/ajuste.txt', sujeto)
     x_val, y_val = ds.leer_datos('./datos/validacion.txt', sujeto)
 
     max_g = 2*x_aju.shape[0]
-    max_l = 200
+    cant_l = 100
 
-    lambdas = [i for i in range(1, max_l)]
+    lambdas = np.logspace(1e-8,1, cant_l)
     grados  = [i for i in range(1, max_g)]
 
     g_opt = -1
     l_opt = -1
-    error = [[0] * max_l for _ in range(max_g)]
+    error = [[0] * cant_l for _ in range(max_g)]
     min_err = 1e8
 
     for g in grados:
-        for l in lambdas:
-            error[g][l] = ds.predecir_con_reg(x_aju, y_aju, x_val, y_val, g, l)
-            if error[g][l] < min_err:
-                min_err = error[g][l]
+        for i,l in enumerate(lambdas):
+            error[g][i] = ds.predecir_con_reg(x_aju, y_aju, x_val, y_val, g, l)
+            if error[g][i] < min_err:
+                min_err = error[g][i]
                 g_opt = g
                 l_opt = l
 
